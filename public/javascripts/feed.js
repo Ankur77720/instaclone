9f6017308ed00d56fdc9d7620064d362b3813e23
Ankur Prajapati <91510065+Ankur77720@users.noreply.github.com>
2023-02-02 15:21:13 +0530

    nothing much

---
+++ public/javascripts/feed.js
@@ -0,0 +1,43 @@
+async function likeIt(postId, element) {
+  let res = await fetch(`/like/${postId}`)
+  res = await res.json()
+  let id = element
+  element = document.querySelector('#' + 'i' + element)
+  if (res) {
+    document.querySelector('#likes').innerText =
+      parseInt(document.querySelector('#likes').innerText) + 1
+    element.classList.remove('ri-heart-3-line')
+    element.classList.add('ri-heart-3-fill', 'text-danger')
+    document.querySelector(`#like${id}`).classList.add('like')
+  } else {
+    document.querySelector('#likes').innerText =
+      parseInt(document.querySelector('#likes').innerText) - 1
+    element.classList.add('ri-heart-3-line')
+    element.classList.remove('ri-heart-3-fill', 'text-danger')
+    document.querySelector(`#like${id}`).classList.remove('like')
+  }
+}
+async function follow(userId) {
+  let res = await fetch(`/follow/${userId}`)
+  res = await res.json()
+
+  let followButtons = document.querySelectorAll(`.follow${userId}`)
+  followButtons.forEach((button) => {
+    if (res) {
+      button.classList.remove('btn-outline-success')
+      button.classList.add('btn-outline-warning')
+      button.innerText = 'Following'
+    } else {
+      button.classList.add('btn-outline-success')
+      button.classList.remove('btn-outline-warning')
+      button.innerText = 'Follow'
+    }
+  })
+  if (res) {
+    document.querySelector('#following').innerText =
+      parseInt(document.querySelector('#following').innerText) + 1
+  } else {
+    document.querySelector('#following').innerText =
+      parseInt(document.querySelector('#following').innerText) - 1
+  }
+}
